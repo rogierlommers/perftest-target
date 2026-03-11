0f8368069c879c46b5bf13d2d862338f404fcbf7
Rogier Lommers <rogier@lommers.org>
2026-03-11 10:00:11 +0100

    Remove strange chars in tag

--- build-and-push.sh
+++ build-and-push.sh
@@ -4,7 +4,7 @@ set -euo pipefail
 
 IMAGE="rogierlommers/perftest"
 PLATFORM="linux/amd64"
-TAG="$(date +"%Y-%m-%d-%H%M")"
+TAG="$(date +"%Y%m%d%H%M")"
 
 echo "Building and pushing Docker image ${IMAGE}:${TAG} for ${PLATFORM}..."
 docker buildx build --platform "${PLATFORM}" -t "${IMAGE}:${TAG}" --push .
